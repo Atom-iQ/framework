306f5b9145b30c302b1dda0279ba908b92ded6ed
Adam Filipek <adamfilipek1@gmail.com>
2022-02-02 20:47:30 +0100

    feat(fx): new @atom-iq/fx package for functional programming utilities

---
+++ packages/fx/rollup.config.js
@@ -0,0 +1,72 @@
+import babel from '@rollup/plugin-babel'
+import ts from '@rollup/plugin-typescript'
+import { terser } from 'rollup-plugin-terser'
+import bundleSize from 'rollup-plugin-bundle-size'
+import sizes from 'rollup-plugin-sizes'
+import gzip from 'rollup-plugin-gzip'
+
+export default () => [
+  // browser-friendly UMD build
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        file: 'dist/index.umd.js',
+        name: 'iQRx',
+        format: 'umd'
+      },
+      {
+        file: 'dist/index.umd.min.js',
+        name: 'iQRx',
+        format: 'umd',
+        plugins: [terser(), gzip()]
+      }
+    ],
+    plugins: [
+      ts(),
+      sizes({
+        details: true
+      }),
+      bundleSize({
+        file: 'src/index.ts'
+      })
+    ]
+  },
+  {
+    input: {
+      index: 'src/index.ts'
+    },
+    output: [
+      {
+        dir: 'dist',
+        entryFileNames: '[name].es.js',
+        format: 'es'
+      },
+      {
+        dir: 'dist',
+        entryFileNames: '[name].es.min.js',
+        format: 'es',
+        plugins: [terser(), gzip()]
+      },
+      {
+        dir: 'dist/lib',
+        entryFileNames: '[name].js',
+        format: 'es',
+        preserveModules: true
+      },
+      {
+        dir: 'dist/lib',
+        entryFileNames: '[name].min.js',
+        format: 'es',
+        preserveModules: true,
+        plugins: [terser(), gzip()]
+      }
+    ],
+    plugins: [
+      ts({
+        declaration: true,
+        declarationDir: 'dist/lib/types'
+      }),
+    ]
+  }
+]
