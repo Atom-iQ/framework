491bc2ca88d1d206c910e41f7f6caa2d1598200e
Adam Filipek <adamfilipek1@gmail.com>
2022-02-26 02:07:24 +0100

    rR

--- packages/core/src/reactive-virtual-dom/renderer/index.ts
+++
@@ -1,1 +0,0 @@
-export { renderRootChild } from './element'
--- packages/core/src/shared/types/reactive-event-delegation/native-events.ts
+++
@@ -1,5 +0,0 @@
-// These types need to be in another file to not conflict with Inferno event type names
-export type NativeClipboardEvent = ClipboardEvent
-export type NativeCompositionEvent = CompositionEvent
-export type NativeDragEvent = DragEvent
-export type NativeFocusEvent = FocusEvent
