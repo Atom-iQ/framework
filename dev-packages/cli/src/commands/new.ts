599bfda56dc067a63b99098ba5069c7875f7d4bc
Adam Filipek <adamfilipek1@gmail.com>
2022-02-26 01:55:20 +0100

    Remove public Atom-iQ repo

--- dev-packages/cli/src/commands/new.ts
+++
@@ -1,6 +0,0 @@
-module.exports = (declareCommand, command, option, alias) => (commandDeclaration: string) => {
-  return declareCommand(command(commandDeclaration))(
-    alias('new'),
-    option('--no-cli', 'Create project with own webpack and babel config, without CLI control')
-  )
-}
--- dev-packages/cli/src/commands/start.ts
+++
@@ -1,7 +0,0 @@
-module.exports = (command, option, alias) => (commandDeclaration: string) => {
-  return option(
-    '-p --port <port>', 'Specify the port for development server to use', {
-      defaultValue: '7777'
-    }
-  )(alias('serve')(command(commandDeclaration)))
-}
